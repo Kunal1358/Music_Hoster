d4bfeee862a95a4f8790a68b8d618a3ee02b5de6
Kunal1358 <kunalkohli8054@gmail.com>
2021-05-25 18:52:54 +0530

    Code Updated

--- src/main/java/com/chitkara/Music_Hoster/repository/MusicRepository.java
+++
@@ -1,9 +0,0 @@
-package com.chitkara.Music_Hoster.repository;
-
-import com.chitkara.Music_Hoster.model.Music;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MusicRepository extends JpaRepository<Music,String>{
-}
