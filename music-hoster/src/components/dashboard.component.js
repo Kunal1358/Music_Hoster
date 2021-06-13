e8368812d7dce9f01ed0591e9f51809ce7a987d0
MrZemu <hbhandari.cse18@chitkarauniversity.edu.in>
2021-06-13 22:56:24 +0530

    added dashboard

---
+++ music-hoster/src/components/dashboard.component.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+//import { useForm } from "react-hook-form";
+import axios from 'axios'
+import Form from'./form.component';
+import Table from './table.component'
+ 
+
+  
+   
+  
+export default function Dashboard() {
+  
+  window.onload=function(){
+    //document.getElementsByClassName("nav-link")[0].style.visibilty='hidden';
+    if (!window.localStorage.getItem("token")) {
+    console.log("redirect to login");
+    window.location.href="http://localhost:3000/Sign-in";}
+    
+};
+
+  return(
+    <div>  
+      <br/>
+      <br/>
+      <h3 className="h3">Add Music</h3><br/>
+      
+    <Form />
+    <br/>
+    <Table/>
+    
+    </div>
+    
+    );
+   
+    
+}
